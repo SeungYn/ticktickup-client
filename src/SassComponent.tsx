73efdfa0265285f948c0e63515447333ac623e2e
SeungYn <kwls0407@gmail.com>
2022-12-24 14:15:46 +0900

    ticktickup프로젝트

---
+++ src/SassComponent.tsx
@@ -0,0 +1,15 @@
+import './SassComponent.scss';
+
+export default function SassComponent() {
+  return (
+    <div className='SassComponent'>
+      <div className='box red'></div>
+      <div className='box orange'></div>
+      <div className='box yellow'></div>
+      <div className='box green'></div>
+      <div className='box blue'></div>
+      <div className='box indigo'></div>
+      <div className='box violet'></div>
+    </div>
+  );
+}
